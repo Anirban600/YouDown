087810e9dfb9b37699c85671403afde4e3761895
Anirban Haldar <anirban.haldar600@gmail.com>
2022-06-23 19:15:59 +0530

    Download Fixed

--- yousuck.py
+++ yousuck.py
@@ -106,5 +106,5 @@ def get_playlist():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
-    # app.run(debug=True, port=process.env.PORT)
+    # app.run(debug=True, port=8080)
+    app.run(debug=True, port=process.env.PORT)
